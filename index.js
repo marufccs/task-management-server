53d305ba44aac94b2e77444db51b847cdd7bdd68
unknown <marufmozumder7@gmail.com>
2022-12-29 13:11:20 +0100

    task-query-added

--- index.js
+++ index.js
@@ -14,13 +14,37 @@ app.get('/', (req, res) => {
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.19qwu6y.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri)
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const client = new MongoClient(uri);
+
+const run = async() => {
+    try{
+        const myTasks = client.db('taskManagement').collection('myTasks');
+
+        //Adding the task to the database
+        app.post('/mytasks', async(req, res) => {
+            const task = req.body;
+            const result = await myTasks.insertOne(task);
+            res.send(result);
+        })
+
+        //Showing the data to the server
+        app.get('/mytasks', async(req, res) => {
+            let query= {};
+            if (req.query.email){
+                query={
+                  email: req.query.email
+                }
+              }
+            const cursor = myTasks.find(query);
+            const tasks = await cursor.toArray();
+            res.send(tasks);
+        })
+    }
+    catch(error){
+        console.log(error.name, error.message, error.stack);
+    }
+}
+run().catch(err => console.error(err))
 
 app.listen(port, () => {
     console.log('its running')
